d52696490622970a1d5b7ce358abd29d314a4b3e
Burak Acar <nburakacar@gmail.com>
2023-04-11 21:03:04 +0300

    Java Hackerrank

---
+++ Java/JavaLambdaExpressions.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+interface PerformOperation {
+ boolean check(int a);
+}
+class MyMath {
+ public static boolean checker(PerformOperation p, int num) {
+  return p.check(num);
+ }
+ public static PerformOperation isOdd() {
+        return number -> (number & 1) == 1;
+    }
+
+    public static PerformOperation isPrime() {
+        return number -> {
+            if (number < 2) {
+                return false;
+            }
+            for (int i = 2; i * i <= number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+    }
+
+    public static PerformOperation isPalindrome() {
+        return number -> {
+            int reversed = 0;
+            int original = number;
+            while (original > 0) {
+                int digit = original % 10;
+                reversed = reversed * 10 + digit;
+                original /= 10;
+            }
+            return number == reversed;
+        };
+    }
+}
+public class JavaLambdaExpressions {
+
+ public static void main(String[] args) throws IOException {
+  MyMath ob = new MyMath();
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  int T = Integer.parseInt(br.readLine());
+  PerformOperation op;
+  boolean ret = false;
+  String ans = null;
+  while (T--> 0) {
+   String s = br.readLine().trim();
+   StringTokenizer st = new StringTokenizer(s);
+   int ch = Integer.parseInt(st.nextToken());
+   int num = Integer.parseInt(st.nextToken());
+   if (ch == 1) {
+    op = ob.isOdd();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "ODD" : "EVEN";
+   } else if (ch == 2) {
+    op = ob.isPrime();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PRIME" : "COMPOSITE";
+   } else if (ch == 3) {
+    op = ob.isPalindrome();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+   }
+   System.out.println(ans);
+  }
+ }
+}
+
